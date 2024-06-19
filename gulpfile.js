eb66c871a833c9a5b02b76fd5892201bc0e0f28f
Qrj <renjie.qin@brain.im>
2024-06-19 14:21:47 +0800

    test(release): test release and publish

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
-import { execSync } from 'child_process'
+import { execSync } from 'child_process';
 
 const codeFormatting = async () => {
-  await execSync('npm run lint', { stdio: 'inherit' })
-}
+  await execSync('npm run lint', { stdio: 'inherit' });
+};
 
 export const build = async () => {
-  await codeFormatting()
-  await execSync('npm run clean:build', { stdio: 'inherit' })
-  await execSync('npx tsup', { stdio: 'inherit' })
-}
+  await codeFormatting();
+  await execSync('npm run clean:build', { stdio: 'inherit' });
+  await execSync('npx tsup', { stdio: 'inherit' });
+};
+
+export const release = async () => {
+  await build();
+
+  await execSync('release-it', { stdio: 'inherit' });
+  await execSync('npm publis', { stdio: 'inherit' });
+};
