627df1f53e044146dda1dd308ee65c218964ba56
QRenjie <renjie.qin@brain.im>
2024-07-03 13:43:34 +0800

    ci: fix tests

--- tsup.config.ts
+++ tsup.config.ts
@@ -19,23 +19,9 @@ export default defineConfig([
   {
     format: ['esm', 'cjs'],
     entry: ['./packages/main/index.ts'],
-    outDir: 'dist/main',
-    platform: 'browser',
-    globalName: 'PackagesMain',
-    external: [],
-    outExtension({ format }) {
-      if (format === 'iife') return { js: '.js' };
-      return { js: `.${format}.js` };
-    },
-    ...commonConfig
-  },
-  {
-    format: ['esm', 'cjs'],
-    entry: ['./packages/node/index.ts'],
-    outDir: 'dist/node',
-    platform: 'node',
-    globalName: 'PackagesNode',
-    external: [],
+    outDir: 'dist',
+    platform: 'neutral',
+    globalName: 'SliceStore',
     outExtension({ format }) {
       if (format === 'iife') return { js: '.js' };
       return { js: `.${format}.js` };
