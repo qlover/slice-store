2dcf01bc6a37c8659e63c7710fc7fb0310de8d77
QRJ <renjie.qin@brain.im>
2025-03-25 09:46:30 +0800

    feat: maker

---
+++ packages/slice-store/src/factory.ts
@@ -0,0 +1,24 @@
+export type ConstructorType<T, Args extends unknown[]> =
+  | (new (...args: Args) => T)
+  | ((...args: Args) => T);
+
+/**
+ * Create an instance of a class or execute a function
+ * @param Constructor - Class constructor or normal function
+ * @param args - Arguments passed to the constructor or function
+ * @returns Instance of the class or function return value
+ * @throws Throws an error when the class instance is created or the function is called
+ */
+export function factory<T, Args extends unknown[]>(
+  Constructor: ConstructorType<T, Args>,
+  ...args: Args
+): T {
+  if (
+    typeof Constructor === 'function' &&
+    Constructor.prototype &&
+    Constructor.prototype.constructor === Constructor
+  ) {
+    return new (Constructor as new (...args: Args) => T)(...args);
+  }
+  return (Constructor as (...args: Args) => T)(...args);
+}
--- packages/slice-store/src/index.ts
+++ packages/slice-store/src/index.ts
@@ -1,2 +1,3 @@
 export * from './Observer';
 export * from './SliceStore';
+export * from './factory';
