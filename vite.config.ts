a9fd6458c0e18cee901d7d6565bf823796b83478
QRenjie <renjie.qin@brain.im>
2025-08-16 18:04:42 +0800

    refactor: update project structure and configurations
    - Changed frontend release branch from 'main' to 'master' in .env and .env.template files.
    - Updated package.json to reflect new project name and versioning.
    - Added new configuration files for changesets and commitlint.
    - Removed outdated release workflows and added a new unified release workflow.
    - Updated dependencies and scripts for improved build and test processes.
    - Refactored Vite configuration for better test environment setup.
    - Added asset management utilities and Rollup configuration for building packages.
    - Updated README files to reflect new project name and structure.
    
    Co-authored-by: QRJ <renjie.qin@brain.im>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  // plugins: [
+  // FIXME: not override alias?
+  //   viteMockPackage({
+  //     onlyPackages: ['slice-store']
+  //   })
+  // ],
   test: {
-    environment: 'jsdom',
+    environment: 'node',
     globals: true,
-    setupFiles: ['./setupTests.ts'],
-    include: ['packages/**/__tests__/**/*.test.{ts,tsx}']
+    watch: false,
+    include: ['packages/**/__tests__/**/*.test.{ts,tsx}'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/.changelog/**',
+      '**/.github/**',
+      '**/.husky/**',
+      '**/.vscode/**',
+      '**/.nx/**',
+    ],
+    alias: {
+      '@qlover/slice-store': './packages/slice-store/__mocks__/index.ts',
+    }
   }
 });
